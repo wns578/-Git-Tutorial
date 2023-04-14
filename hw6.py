b1510b4e37835eaf121b32675bf6cb8e6934b828
wns578 <127467761+wns578@users.noreply.github.com>
2023-04-14 15:10:58 +0900

    Add files via upload

---
+++ hw6.py
@@ -0,0 +1,22 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[1]:
+
+
+for i in range(1, 10) :
+    for j in range (2, 6) :
+        print(f'{j} × {i} = {i*j:2d}',end = '\t')
+    print()
+print()
+for i in range(1, 10) :
+    for j in range (6, 10) :
+        print(f'{j} × {i} = {i*j:2d}',end = '\t')
+    print()
+
+
+# In[ ]:
+
+
+
+
